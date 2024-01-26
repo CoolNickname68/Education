20392365377d95ed44df5b3a5017d4d1b1b060d6
qwerty <dima.dmitri.vladimirov@mail.ru>
2024-01-26 20:21:28 +0300

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,154 +3,136 @@ public class Main {
 
 
     public static void main(String[] args) {
-        byte fish = 14;
-        int dog = 35000;
-        short bird = 400;
-        long whale = 900000000000000000L;
-        float cat = 32.34123F;
-        double lion = 31.123548971;
+        Main mainInstance = new Main();
+        Task1 task1Object = mainInstance.new Task1();
+        task1Object.printAgeMessage();
 
-        System.out.println("Fish: " + fish + ", Dog: " + dog + ", Bird: " + bird + ", Whale: " + whale + ", Cat: " + cat + ",Lion: " + lion);
+        Task2 task1Object2 = mainInstance.new Task2();
+        task1Object2.task();
 
-
-        double doubleValue = 27.12;
-        long longValue = 987678965549L;
-        int intValue = 2786;
-        short shortValue = 569;
-        int negativeValue = -159;
-        int anotherIntValue = 27897;
-        byte byteValue = 67;
-
-        System.out.println("Double Value: " + doubleValue);
-        System.out.println("Long Value: " + longValue);
-        System.out.println("Int Value: " + intValue);
-        System.out.println("Short Value: " + shortValue);
-        System.out.println("Negative Value: " + negativeValue);
-        System.out.println("Another Int Value: " + anotherIntValue);
-        System.out.println("Byte Value: " + byteValue);
+        Task3 task1Object3 = mainInstance.new Task3();
+        task1Object3.task1();
 
 
-        int studentsInClass1 = 23;
-        int studentsInClass2 = 27;
-        int studentsInClass3 = 30;
-
-        // Общее количество листов бумаги
-        int totalSheets = 480;
-
-        // Рассчитываем количество листов бумаги для каждого ученика в каждом классе
-        int sheetsPerStudentInClass1 = totalSheets / studentsInClass1;
-        int sheetsPerStudentInClass2 = totalSheets / studentsInClass2;
-        int sheetsPerStudentInClass3 = totalSheets / studentsInClass3;
-
-        // Выводим результат в консоль
-        System.out.println("На каждого ученика в первом классе рассчитано " + sheetsPerStudentInClass1 + " листов бумаги");
-        System.out.println("На каждого ученика во втором классе рассчитано " + sheetsPerStudentInClass2 + " листов бумаги");
-        System.out.println("На каждого ученика в третьем классе рассчитано " + sheetsPerStudentInClass3 + " листов бумаги");
-
-        // Производительность машины (бутылок за 2 минуты)
-        int bottlesPer2Minutes = 16;
-
-        // Рассчитываем производительность машины за разные промежутки времени
-        int bottlesPer20Minutes = bottlesPer2Minutes * 10;
-        int bottlesPerDay = bottlesPer2Minutes * (24 * 60 / 2);
-        int bottlesPer3Days = bottlesPerDay * 3;
-        int bottlesPerMonth = bottlesPerDay * 30;
-
-        // Выводим результат в консоль
-        System.out.println("За 20 минут машина произвела " + bottlesPer20Minutes + " штук бутылок");
-        System.out.println("В сутки машина произвела " + bottlesPerDay + " штук бутылок");
-        System.out.println("За 3 дня машина произвела " + bottlesPer3Days + " штук бутылок");
-        System.out.println("За 1 месяц машина произвела " + bottlesPerMonth + " штук бутылок");
-        
-
-        // Общее количество банок краски для ремонта
-        int totalPaintCans = 120;
-
-        // Количество банок краски для одного класса
-        int whitePaintPerClass = 2;
-        int brownPaintPerClass = 4;
-
-        // Количество классов
-        int numberOfClasses = totalPaintCans / (whitePaintPerClass + brownPaintPerClass);
-
-        // Рассчитываем количество банок каждого цвета краски
-        int whitePaintCans = numberOfClasses * whitePaintPerClass;
-        int brownPaintCans = numberOfClasses * brownPaintPerClass;
-
-        // Выводим результат в консоль
-        System.out.println("В школе, где " + numberOfClasses + " классов, нужно " +
-                whitePaintCans + " банок белой краски и " +
-                brownPaintCans + " банок коричневой краски");
-        
-
-    
-                int weightOfBanana = 80;
-                int quantityOfBanana = 5;
-                int weightOfMilk = 105; // 100 мл молока = 105 грамм
-                int quantityOfMilk = 200 / weightOfMilk; // Общий объем молока в миллилитрах
-                int weightOfIceCream = 100;
-                int quantityOfIceCream = 2;
-                int weightOfEgg = 70;
-                int quantityOfEgg = 4;
-        
-                // Рассчитываем общий вес завтрака в граммах
-                int totalWeightInGrams = (weightOfBanana * quantityOfBanana) +
-                        (weightOfMilk * quantityOfMilk) +
-                        (weightOfIceCream * quantityOfIceCream) +
-                        (weightOfEgg * quantityOfEgg);
-        
-                // Рассчитываем общий вес завтрака в килограммах
-                double totalWeightInKilograms = totalWeightInGrams / 1000.0;
-        
-                // Выводим результат в консоль
-                System.out.println("Вес завтрака: " + totalWeightInGrams + " грамм");
-                System.out.println("Вес завтрака: " + totalWeightInKilograms + " килограмм");
+        Task4 task1Object4 = mainInstance.new Task4();
+        task1Object4.task2();
 
 
-    
+        Task5 task1Object5 = mainInstance.new Task5();
+        task1Object5.task3();
 
+        Task6 task1Object6 = mainInstance.new Task6();
+        task1Object6.task4();
 
-        // Вес, который нужно сбросить
-        int weightToLose = 7;
-
-        // Диапазон потери веса в граммах
-        int minWeightLossPerDay = 250;
-        int maxWeightLossPerDay = 500;
-
-        // Рассчитываем количество дней для каждого варианта потери веса
-        int daysToLoseMinWeight = weightToLose / minWeightLossPerDay;
-        int daysToLoseMaxWeight = weightToLose / maxWeightLossPerDay;
-
-        // Рассчитываем среднее количество дней
-        int averageDaysToLoseWeight = (daysToLoseMinWeight + daysToLoseMaxWeight) / 2;
-
-        // Выводим результат в консоль
-        System.out.println("Дней для потери 7 кг при потере 250 грамм в день: " + daysToLoseMinWeight + " дней");
-        System.out.println("Дней для потери 7 кг при потере 500 грамм в день: " + daysToLoseMaxWeight + " дней");
-        System.out.println("Среднее количество дней для потери 7 кг: " + averageDaysToLoseWeight + " дней");
-
-
-
-        // Зарплаты сотрудников до повышения
-        double salaryMasha = 67760;
-        double salaryDenis = 83690;
-        double salaryKristina = 76230;
-
-        // Коэффициент повышения
-        double increasePercentage = 0.10;
-
-        // Повышение зарплаты и расчет годового дохода
-        double newSalaryMasha = salaryMasha * (1 + increasePercentage);
-        double newSalaryDenis = salaryDenis * (1 + increasePercentage);
-        double newSalaryKristina = salaryKristina * (1 + increasePercentage);
-
-        double annualIncomeIncreaseMasha = (newSalaryMasha - salaryMasha) * 12;
-        double annualIncomeIncreaseDenis = (newSalaryDenis - salaryDenis) * 12;
-        double annualIncomeIncreaseKristina = (newSalaryKristina - salaryKristina) * 12;
-
-        // Вывод результатов в консоль
-        System.out.printf("Маша теперь получает %.2f рублей. Годовой доход вырос на %.2f рублей.%n", newSalaryMasha, annualIncomeIncreaseMasha);
-        System.out.printf("Денис теперь получает %.2f рублей. Годовой доход вырос на %.2f рублей.%n", newSalaryDenis, annualIncomeIncreaseDenis);
-        System.out.printf("Кристина теперь получает %.2f рублей. Годовой доход вырос на %.2f рублей.%n", newSalaryKristina, annualIncomeIncreaseKristina);
+        Task7 task1Object7 = mainInstance.new Task7();
+        task1Object7.task5();
     }
+    
+
+    public class Task1{
+        public void printAgeMessage() {
+            int age = 18;
+    
+            if (age >= 18) {
+                System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+            } else {
+                System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+            }
+        }
+    }
+
+    public class Task2 {
+        public void task() {
+            int temperature = 7;
+    
+            if (temperature < 5) {
+                System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+            } else {
+                System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+            }
+        }
+    }
+
+    public class Task3 {
+        public void task1() {
+            int speed = 65;
+    
+            if (speed > 60) {
+                System.out.println("Если скорость " + speed + " км/ч, то придется заплатить штраф");
+            } else {
+                System.out.println("Если скорость " + speed + " км/ч, то можно ездить спокойно");
+            }
+        }
+    }
+
+    public class Task4 {
+        public void task2() {
+            int age = 20;
+    
+            if (age >= 2 && age <= 6) {
+                System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
+            } else if (age >= 7 && age <= 17) {
+                System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
+            } else if (age >= 18 && age <= 24) {
+                System.out.println("Если возраст человека равен " + age + ", то его место в университете");
+            } else {
+                System.out.println("Если возраст человека равен " + age + ", то ему пора ходить на работу");
+            }
+        }
+    }
+
+    public class Task5 {
+        public void task3() {
+            int age = 10;
+    
+            if (age < 5) {
+                System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе");
+            } else if (age >= 5 && age < 14) {
+                System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе в сопровождении взрослого");
+            } else {
+                System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе без сопровождения взрослого");
+            }
+        }
+    }
+
+    public class Task6 {
+        public void task4() {
+            int totalPeople = 90;
+            int seatedPlaces = 60;
+    
+            if (totalPeople < 102) {
+                System.out.println("В вагоне есть место");
+                if (totalPeople <= seatedPlaces) {
+                    System.out.println("Место сидячее");
+                } else {
+                    System.out.println("Место стоячее");
+                }
+            } else {
+                System.out.println("В вагоне уже полностью забито");
+            }
+        }
+    }
+
+    public class Task7 {
+        public void task5() {
+            int one = 15;
+            int two = 8;
+            int three = 20;
+    
+            if (one >= two && one >= three) {
+                System.out.println("Наибольшее число: " + one);
+            } else if (two >= one && two >= three) {
+                System.out.println("Наибольшее число: " + two);
+            } else {
+                System.out.println("Наибольшее число: " + three);
+            }
+        }
+    }
+
+
+
 }
+
+
+
+
