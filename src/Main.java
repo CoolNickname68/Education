94267f4d07ed63a7733fc826769929b281575563
CoolNickname68 <58376127+CoolNickname68@users.noreply.github.com>
2024-02-17 13:02:23 +0300

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,90 @@
-import java.time.LocalDate;
-
-public class Main {
-
+class Main {
     public static void main(String[] args) {
-        // Задача 1
-        int yearToCheck = 2021;
-        checkLeapYear(yearToCheck);
+        // Создание объектов авторов
+        Author author1 = new Author("Иван", "Иванов");
+        Author author2 = new Author("Петр", "Петров");
 
-        System.out.println(); // Пустая строка для разделения вывода задач
+        // Создание объектов книг
+        Book book1 = new Book("Название книги 1", author1, 2000);
+        Book book2 = new Book("Название книги 2", author2, 2010);
 
-        // Задача 2
-        int clientDeviceYear = 2015;
-        int osType = 0; // 0 для iOS, 1 для Android
-        installApp(clientDeviceYear, osType);
+        // Вывод информации о книгах до изменения года публикации
+        System.out.println("Информация о книгах до изменения года публикации:");
+        System.out.println(book1);
+        System.out.println(book2);
 
-        System.out.println(); // Пустая строка для разделения вывода задач
+        // Изменение года публикации одной из книг
+        book1.setPublicationYear(2005);
 
-        // Задача 3
-        int deliveryDistance = 95;
-        int deliveryDays = calculateDeliveryDays(deliveryDistance);
-        System.out.println("Потребуется дней: " + deliveryDays);
-    }
-
-    // Задача 1
-    public static void checkLeapYear(int year) {
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            System.out.println(year + " год — високосный год");
-        } else {
-            System.out.println(year + " год — невисокосный год");
-        }
-    }
-
-    // Задача 2
-    public static void installApp(int clientDeviceYear, int osType) {
-        int currentYear = LocalDate.now().getYear();
-
-        if (clientDeviceYear < currentYear) {
-            if (clientDeviceYear < 2015) {
-                if (osType == 0) {
-                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                } else if (osType == 1) {
-                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
-                }
-            } else {
-                System.out.println("Установите обычную версию приложения");
-            }
-        } else {
-            System.out.println("Установите обычную версию приложения");
-        }
-    }
-
-    // Задача 3
-    public static int calculateDeliveryDays(int deliveryDistance) {
-        if (deliveryDistance <= 20) {
-            return 1;
-        } else if (deliveryDistance <= 60) {
-            return 2;
-        } else if (deliveryDistance <= 100) {
-            return 3;
-        } else {
-            return 0; // Если расстояние больше 100 км, то доставка не предусмотрена
-        }
+        // Вывод информации о книгах после изменения года публикации
+        System.out.println("\nИнформация о книгах после изменения года публикации:");
+        System.out.println(book1);
+        System.out.println(book2);
     }
 }
+
+class Book {
+    private String title;
+    private Author author;
+    private int publicationYear;
+
+    // Конструктор
+    public Book(String title, Author author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    // Геттеры
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    // Сеттер для поля "Год публикации"
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Книга: " + title + ", Автор: " + author.getFullName() + ", Год публикации: " + publicationYear;
+    }
+}
+
+class Author {
+    private String firstName;
+    private String lastName;
+
+    // Конструктор
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Геттеры
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Метод для получения полного имени автора
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
+
 
 
 
